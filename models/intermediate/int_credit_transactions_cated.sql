a5eba6174f14dd08818c69f83560e766b04005bb
MAILE-lana92 <maile.lxm@gmail.com>
2025-03-04 15:36:43 +0700

    update chart and cashflow table

--- models/intermediate/int_credit_transactions_cated.sql
+++ models/intermediate/int_credit_transactions_cated.sql
@@ -17,10 +17,10 @@
     , trans.trans_desc
     , trans.credit
     , case when trans.credit_transaction_type = 'Other' then
-          case when trans.trans_desc like 'LE XUAN MAI%' or trans.trans_desc like ('MAI LE%') then 
+          -- case when trans.trans_desc like 'LE XUAN MAI%' or trans.trans_desc like ('MAI LE%') then 
                case when (cast(trans.trans_date as date) <= date'2023-04-01') and trans.credit >= 1000000 then 'Wedding_spending'
                		when (cast(trans.trans_date as date)>= date'2024-01-01' and cast(trans.trans_date as date) <= date'2024-11-01') and trans.credit >= 2000000 then 'House_improvement' else 'Other' end
-          else trans.credit_transaction_type  end
+          -- else trans.credit_transaction_type  end
           else trans.credit_transaction_type end as credit_transaction_type 
   FROM credit_trans_1 trans
   )
--- models/marts/cash_flow.sql
+++ models/marts/cash_flow.sql
@@ -8,16 +8,23 @@ with income as (
 spending as (
     select
     trans_date
-    ,sum(spending_amount) as amount
+    ,-1*sum(spending_amount) as amount
     from {{ ref('spending') }} spending
     group by trans_date
 ),
 investment as (
     select 
     trans_date
-    , sum(investment) as amount
+    ,-1*sum(investment) as amount
+    ,sum(investment_return) as return_amount
     from {{ ref('investment')}} investment
     group by trans_date
+),
+balance as (
+    select 
+    trans_date
+    ,balance
+    from {{ ref('balance')}}
 ),
 cte1 as (
     select  case when income.trans_date is not null then income.trans_date
@@ -26,19 +33,23 @@ cte1 as (
     	else investment.trans_date
     	end
     	end as trans_date
+        ,balance.balance
         ,income.amount as income_amount
         ,spending.amount as spending_amount
         ,investment.amount as investment_amount
+        ,investment.return_amount as investment_return_amount
     from income
     full outer join spending
     on income.trans_date = spending.trans_date
     full outer join investment
     on income.trans_date = investment.trans_date
+    full outer join balance
+    on income.trans_date = balance.trans_date
 ),
 cte as (
 select 
 cte1.*
-,cte1.income_amount - cte1.spending_amount - cte1.investment_amount as net_income
+,cte1.income_amount + cte1.spending_amount + cte1.investment_amount + cte1.investment_return_amount as net_income
 from cte1
 )
 select ROW_NUMBER() OVER (ORDER by trans_date) as id
